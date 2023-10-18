ac3fcb62eeeb532d2c52decdeae336756945bdfd
Your Name <you@example.com>
2023-10-18 12:03:39 +0700

    first commit

---
+++ WPF TEST/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_TEST
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        string result = string.Empty;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            result += ((Button)sender).Content;
+            UpdateText();
+
+        }
+
+
+        private void UpdateText() 
+        {
+            textBlock.Text = result;
+        }
+
+    }
+}
